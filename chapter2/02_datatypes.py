a376615c1a9fc99926a7f2338246707d1b16f963
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-05 21:46:50 +0530

    Add files via upload

---
+++ chapter2/02_datatypes.py
@@ -0,0 +1,10 @@
+a=1  # a is an integar
+
+b=5.22 # b is a floating ponit number
+
+c="iz-snippet"  # c is a string
+
+d=True  # d is a boolean variable
+
+e=None  # e is a NoneType variable
+
---
+++ chapter2/o3_operators.py
@@ -0,0 +1,32 @@
+#arithmatic operators
+
+a=34
+b=12
+c=a+b 
+print(c)
+
+# assinment operators
+
+a=84-66 #assinging the value of 84-66 to a
+print(a)
+b=7
+b+= 10 #adding 10 to the value of b and assing it to b
+print(b)
+
+# comparison operators
+k=2<4
+print(k)  
+
+l=5!=5
+print(l)
+
+# logical operators
+m=True or False
+print(m)
+
+#truth table for logical operators
+# A      B      A and B     A or B     not A
+# True   True   True        True       False
+# True   False  False       True       False
+# False  True   False       True       True
+
