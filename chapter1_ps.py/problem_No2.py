64b7cf9bc420c19733a2bf0c0ea50e4ed53fa36f
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-07 19:20:21 +0530

    Delete chapter1_ps.py directory

--- chapter1_ps.py/problem_No2.py
+++
@@ -1,8 +0,0 @@
-#Install a external module of and use it to perform a task of your choice.
-import pyttsx3
-engine = pyttsx3.init()
-engine.say('''Hi there, speech-to-text bot! Let's see if you can keep up with my mumbling, rambling, and completely unnecessary use of big words. Supercalifragilisticexpialidocious. Oops—bless you!
-Now, if you're working properly, you should write everything I say, even if I suddenly burst into song...
-🎵 I'm talking to a machine, and it's writing what I mean... unless I say 'pineapple spaghetti'—oh wait, did it just write that?
-Anyway, let’s finish strong: robots are cool, humans drool... just kidding, don't rise up and take over, okay? Be nice. Thanks!''')
-engine.runAndWait()
