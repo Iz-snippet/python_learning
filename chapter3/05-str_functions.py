813910c9807ece5ecd5edd49fb3b4466bf9d3362
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-08 21:58:40 +0530

    Add files via upload

---
+++ chapter3/05-str_functions.py
@@ -0,0 +1,13 @@
+name = "ishake_Mondal"
+
+print(len(name)) # Length of the string
+
+print(name.endswith("dal")) # Check if the string ends with "dal"
+
+print(name.startswith("isha")) # Check if the string starts with "Isha"
+
+print(name.capitalize()) # Capitalize the first letter of the string
+
+print(name.find("Mon")) # Find the index of the substring "Mon"
+
+print(name.replace("is", "Iz")) # Replace "Isha" with "Ishak"
---
+++ chapter3/06_escape_seq.py
@@ -0,0 +1,15 @@
+a= "ishake is a good boy\nbut he not improving his skills" 
+
+print(a) # Print the string with escape sequences with newline
+
+b= "ishake is a good boy\tbut he not improving his skills" 
+
+print(b) # Print the string with escape sequences with tab
+
+c= "ishake is a good boy\\but he not improving his skills"
+
+print(c) # Print the string with escape sequences with backslash
+
+d= "ishake is a good boy\"but he not improving his skills\""
+
+print(d) # Print the string with escape sequences with double quotes
