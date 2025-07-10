6b45f67fe4d576b1a0b3130b0defd234a82d015f
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-10 22:14:34 +0530

    Update 06_problem.py

--- chapter5_ps/06_problem.py
+++ chapter5_ps/06_problem.py
@@ -3,4 +3,26 @@
 # use keys as their names . Asumme that the
 # names are unique
 
+dic={}
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+print(dic)
 
