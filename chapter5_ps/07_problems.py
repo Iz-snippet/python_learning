1ec329a426f8d074da2c23e55960e08b2ab2856b
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-10 22:19:01 +0530

    Update 07_problems.py

--- chapter5_ps/07_problems.py
+++ chapter5_ps/07_problems.py
@@ -1,1 +1,26 @@
 # if the name of the two friends are same what will be the output of question 6?
+dic={}
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+print(dic)
+
+# Ans:-
+# the same name launague will update and the last update data will count
