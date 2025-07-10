fdc1a7f268d2a1566dd2858965d5783574a637a8
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-10 22:31:15 +0530

    Update 09_problem.py

--- chapter5_ps/09_problem.py
+++ chapter5_ps/09_problem.py
@@ -1,2 +1,13 @@
 # can you change  the values inside a list which contain as s
 # s={8,7,12,"Ishake",[1,2]}
+
+s={8,7,12,"Ishake",[1,2]}
+
+# ans:-
+# This will raise an error:
+
+# TypeError: unhashable type: 'list'
+
+# Sets in Python (set) can only contain immutable (hashable) items.
+
+# Lists are mutable, so they cannot be added to a set.
