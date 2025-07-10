c8feeb10541f27d7cba98dbaccc18a1ffc3c464a
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-10 22:21:49 +0530

    Update 08_problem.py

--- chapter5_ps/08_problem.py
+++ chapter5_ps/08_problem.py
@@ -1,1 +1,25 @@
 # if the language of the two friends is same what will happend  the output of question 6?
+dic={}
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+name= input("enter the friends name:-")
+lang= input("enter the language name:- ")
+
+dic.update({name:lang})
+
+print(dic)
+
+# if the value is same that not affect in the program
