6b27181c4327f895419c0d9ecc814a347c038a4d
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-11 15:42:23 +0530

    Update 07problem.py

--- chapter6_ps/07problem.py
+++ chapter6_ps/07problem.py
@@ -1,1 +1,9 @@
-# write a program to find out whether a given post is talking about "ishake" or not
+# write a program to find out whether a given post is talking about "ishake" or not 
+
+post=input("enter your post :-")
+if("Ishake".lower() in post.lower()):
+    print("this post is talking about ishake")
+else:
+    print("this post is not talking about ishake")
+
+#use lower for comparison both in lower case also
