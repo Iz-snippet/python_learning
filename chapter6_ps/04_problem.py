6f6077cc40844b18c1e04de8fb54f298f82735ce
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-11 15:13:37 +0530

    Update 04_problem.py

--- chapter6_ps/04_problem.py
+++ chapter6_ps/04_problem.py
@@ -1,1 +1,8 @@
-# write a program to find whether a given username is present in a a list or not
+# write a program to find whether a given username is 10 characters long or not
+
+username= input("enter your user name:- ")
+
+if(len(username)>10):
+    print("your user name contain 10 characters")
+else:
+    print("your user name not contain 10 characters")
