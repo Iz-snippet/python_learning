949499c46d83b12d9ec98fd8b95584ab73b92ae4
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-11 15:28:05 +0530

    Update 06_problem.py

--- chapter6_ps/06_problem.py
+++ chapter6_ps/06_problem.py
@@ -3,5 +3,21 @@
 # 80-90:- A
 # 70-80:- B
 # 60-70:- C
-# 50_60:- D
+# 50-60:- D
 # <50  :- F
+
+
+marks= int(input("enter your marks:- "))
+
+if(marks<=100 and marks>=90):
+    print("you get  Ex grade ")
+elif(marks<=90 and marks>=80):
+    print("you get  A grade ")
+elif(marks<=80 and marks>=70):
+    print("you get  B grade ")
+elif (marks<=70 and marks>=60):
+    print("you get  C grade ")
+elif(marks<=60 and marks>=50):
+    print("you get  D grade ")
+else:
+    print("you are failed in the exam")
