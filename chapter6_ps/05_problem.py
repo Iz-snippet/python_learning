d7c57c1fc9d0286e4a5ad14791f9b1cb2b046529
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-11 15:28:38 +0530

    Update 05_problem.py

--- chapter6_ps/05_problem.py
+++ chapter6_ps/05_problem.py
@@ -1,1 +1,11 @@
 # write a program to find whether a given name is present in list or not
+
+list=["ishake","harry","divya","subham","rohan"]
+
+name= input("enter your name please:- ")
+
+if(name in list):
+    print("your name is under the list")
+
+else:
+    print("your name is not in the list")
