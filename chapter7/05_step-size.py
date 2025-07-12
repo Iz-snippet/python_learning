43ee23f37e86657995085daa89b4cb4157cacc81
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-12 20:37:39 +0530

    Add files via upload

---
+++ chapter7/05_step-size.py
@@ -0,0 +1,11 @@
+for i in range(0,100,2):
+    print(i)
+
+# Step size refers to the interval or jump between values when you're iterating over a sequence, especially using range() or slicing.
+
+# start: Where to begin (inclusive)
+
+# stop: Where to stop (exclusive)
+
+# step: The amount to increase or decrease each time (default is +1)
+
---
+++ chapter7/06_for-loop-iterate.py
@@ -0,0 +1,14 @@
+l= [57, 12, 89, 34, 76, 5, 23, 68, 91, 45]
+
+for i in l:
+    print (i)
+
+#for tuple
+t = (17, 64, 82, 9, 43, 28, 91, 36, 59, 73)
+for i in t:
+    print(i)
+
+#for strings
+s="Ishake"
+for i in s:
+  print(i)
