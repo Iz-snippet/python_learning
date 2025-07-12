3452dad3448111f9ec4a3dd8d663495f0f9eb630
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-12 21:16:22 +0530

    Add files via upload

---
+++ chapter7/08_break _and_continue.py
@@ -0,0 +1,12 @@
+#break
+for i in range(100):
+    if(i==8):
+        break # Exit the loop right now
+    print(i)
+
+# continue
+for i in range(100):
+    if(i==8):
+     continue # skip this iteration
+    print(i)
+
