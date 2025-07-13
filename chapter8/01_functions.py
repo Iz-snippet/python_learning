3f9fc95c4bbaf6fd8c4d97994d8eb9a3c6281b5e
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-13 16:59:52 +0530

    Add files via upload

---
+++ chapter8/01_functions.py
@@ -0,0 +1,28 @@
+# a= 13
+# b=43
+# c=56
+# avarage=(a+b+c)/3
+# print(avarage)
+
+
+# a= 153
+# b=3
+# c=5
+# avarage=(a+b+c)/3
+# print(avarage)
+
+# the faster way to do this
+
+def avg():                                            # this is Function Defination
+  d=int(input("enetr the number :-"))
+  b=int(input("enetr the number :-"))
+  c=int(input("enetr the number :-"))
+  avarage = (d+b+c)/3
+  print(avarage)
+
+avg()                                                # this is function call
+avg()
+avg()
+avg()
+avg()
+avg()
---
+++ chapter8/02_quiz.py
@@ -0,0 +1,6 @@
+#write a program to greet good morning to user
+
+def gd():
+    name=input("enter your name:-")
+    print(f"Good Morning {name}")
+gd()
