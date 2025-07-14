c55262a36519e6ca9348d794dcdf7e1abb7cd009
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-14 09:24:46 +0530

    Add files via upload

---
+++ chapter8/04_default_parameter_argument.py
@@ -0,0 +1,5 @@
+def GoodDay(name, ending="Thank You"):
+    print(f"Good Day,{name}")
+    print(ending)
+GoodDay("Ishake", "thanks")  # if i dont update the ending by default ending will be "Thank You"
+GoodDay("Jit")
