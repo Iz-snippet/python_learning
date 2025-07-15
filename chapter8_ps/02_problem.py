c8bb65b9ac213b4d3392ea9cc0f94d9d6f8d89ef
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-15 19:57:50 +0530

    Add files via upload

---
+++ chapter8_ps/02_problem.py
@@ -0,0 +1,7 @@
+# write a program to convert celcius to fahrenheit using function
+
+def ctof():
+    c=int(input("enter the temparature:-"))
+    f= ((c*9)/5)+32
+    print(f"the fahrenheit value is :- {f}")
+ctof()
