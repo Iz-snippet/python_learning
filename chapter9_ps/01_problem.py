1f42acb6412b71c10fe5d7bc9bb5645151206e4f
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-18 20:33:35 +0530

    Update 01_problem.py

--- chapter9_ps/01_problem.py
+++ chapter9_ps/01_problem.py
@@ -1,5 +1,13 @@
 # write a program  to read the text from a given file"poem.txt" and find
-# out whether it contains the word "twinkle"
+# out whether it contains the word "wander"
 
 with open("poem.txt") as f:
-    f.read()
+    c= f.read()
+    if("wander" in c):
+        print("The word wander is present in the content")
+    else:
+        print("The word Wander is not present in the content")
+
+
+
+
