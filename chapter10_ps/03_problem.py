9ead478dadca3533c76ad65bd358e4b72696fbd0
Ishake Mondal <ishakemondal712@gmail.com>
2025-07-21 10:42:56 +0530

    Update 03_problem.py

--- chapter10_ps/03_problem.py
+++ chapter10_ps/03_problem.py
@@ -1,1 +1,12 @@
-# Create a class with a class attribute a; create an object from it and set a directly using object: a = 0. Does this change the class attribute?
+# Create a class with a class attribute a; 
+# create an object from it and set a directly using object: a = 0. Does this change the class attribute?
+
+class demo:
+    a=4
+
+o=demo()
+print(o.a) # prints the class  atrribute because instance atrribute is not here
+o.a=0
+print(o.a) #prints the instance attribute because instance attribute is here now
+
+print(demo.a) # its doesnot effect the class attribute
