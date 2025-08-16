42ccf0aaf51486bdacd4ecc90b3248bfebc60903
Ishake Mondal <ishakemondal712@gmail.com>
2025-08-16 09:05:56 +0530

    Update 04_problem.py

--- chapter11_ps/04_problem.py
+++ chapter11_ps/04_problem.py
@@ -1,17 +1,26 @@
-#Write a class Vector representing a vector of n dimension. Overload the + and * operators which calculate the sum and the dot product of them
+#Write a class complex to represent complex numbers dimension .overload the + and * operator which calculate the sum and the dot product of them
 
 class Complex:
-    def __init__(self, r,i):
-        self.r=r
-        self.i=i
+    def __init__(self, r, i):
+        self.r = r  # real part
+        self.i = i  # imaginary part
 
-    def  __add__(self,c2):
-        return Complex(self.r+c2.r,self.i+c2.i)
+    def __add__(self, c2):
+        return Complex(self.r + c2.r, self.i + c2.i)
+
+    def __mul__(self, c2):
+        # (a + bi)(c + di) = (ac - bd) + (ad + bc)i
+        real = self.r * c2.r - self.i * c2.i
+        imag = self.r * c2.i + self.i * c2.r
+        return Complex(real, imag)
 
     def __str__(self):
         return f"{self.r} + {self.i}i"
 
 
-c1=Complex(1,2)
-c2=Complex(3,4)
-print(c1+c2)
+# Example usage
+c1 = Complex(1, 2)
+c2 = Complex(3, 4)
+
+print("Addition:", c1 + c2)
+print("Multiplication:", c1 * c2)
