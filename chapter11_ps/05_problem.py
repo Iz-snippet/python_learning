9d18bf23b35629f6c82090c302b9af299538c8a1
Ishake Mondal <ishakemondal712@gmail.com>
2025-08-16 09:16:52 +0530

    Update 05_problem.py

---
+++ chapter11_ps/05_problem.py
@@ -0,0 +1,26 @@
+# Write a class Vector representing a vector of n dimension. Overload the + and * operators which calculate the sum and the dot product of them.
+
+class Vector:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+
+    def __add__(self,other):
+        result = Vector(self.x+other.x,self.y+other.y,self.z+other.z)
+        return result
+
+    def __mul__(self,other):
+        result= self.x*other.x + self.y*other.y + self.z*other.z
+        return result
+    def __str__(self):
+        return f"vector({self.x}, {self.y}, {self.z})"
+    
+# Test the implementation
+v1=Vector(1,2,3)
+v2=Vector(4,5,6)
+v3=Vector(7,8,9)
+print(v1+v2)
+print(v1*v2)
+print(v1+v3)
+print(v1*v3)
