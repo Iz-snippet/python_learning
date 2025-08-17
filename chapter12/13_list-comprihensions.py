42faffc540c378965795ec1e6d15ecc84d19124e
Ishake Mondal <ishakemondal712@gmail.com>
2025-08-17 21:52:14 +0530

    Add files via upload

---
+++ chapter12/13_list-comprihensions.py
@@ -0,0 +1,11 @@
+mylist=[1,2,9,3,4,5]
+
+# squaredList=[]
+# for item in mylist:
+#     squaredList.append(item**2)
+
+
+
+# you can do this in List comprihensions function like this:
+squaredList=[i**2 for i in mylist]
+print(squaredList)
